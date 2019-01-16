28a88c231003b7c0d33f9adb8ba3fe3ff8a6fc06
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2019-01-16 08:54:50 -0700

    changes made to clean up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,48 +6,47 @@ var score = 0;
 var t = 200;
 var questions = [{
     'question': 'What does Ty Webb call a doughnut without a hole?',
-    'option1': 'Black And White Cookie', 
-    'option2': 'Cake', 
-    'option3': 'Danish', 
+    'option1': 'Black And White Cookie',
+    'option2': 'Cake',
+    'option3': 'Danish',
     'option4': 'Golf Ball',
     'answer': '3'
-    }, {
+}, {
     'question': 'What does Carl say he will get on his death-bed?',
-    'option1': 'Head Greens-Keeper Job', 
+    'option1': 'Head Greens-Keeper Job',
     'option2': 'Total Consciousness',
     'option3': 'X-Ray Vision',
     'option4': 'Free Ticket To Heaven',
     'answer': '2'
-    }, {
+}, {
     'question': 'What does Carl use to clean up his place after Ty hits his ball?',
-    'option11': 'Leaf-Blower', 
-    'option2': 'Shovel', 
-    'option3': 'His Hands', 
+    'option11': 'Leaf-Blower',
+    'option2': 'Shovel',
+    'option3': 'His Hands',
     'option4': 'Rake',
     'answer': '1'
-    }, {
+}, {
     'question': 'What son of the Dali Lama did Carl caddy for when he was in Tibet?',
-    'option1': '11th', 
+    'option1': '11th',
     'option2': '14th',
-    'option3': '13th', 
+    'option3': '13th',
     'option4': '12th',
     'answer': '3'
-    }, {
+}, {
     'question': 'Who made the gopher in the movie Caddyshack?',
     'option1': 'George Lucas',
-    'option2': 'Harold Ramis', 
-    'option3': 'Steven Spielberg', 
+    'option2': 'Harold Ramis',
+    'option3': 'Steven Spielberg',
     'option4': 'Computer',
     'answer': '1'
-    }, {
+}, {
     'question': 'What does Rodney Dangerfield hit with the golf ball before it bounces back and hits his shoulder?',
     'option1': 'A Tree',
     'option2': 'A Fire Hydrant',
     'option3': 'A Ball Cleaner',
     'option4': 'Chevy Chase',
     'answer': '1'
-    }
-    ]
+}]
 var container = document.getElementById('quizContainer');
 var questionEl = document.getElementById('question');
 var opt1 = document.getElementById('opt1');
@@ -70,40 +69,40 @@ function loadQuestion(questionIndex) {
 
 function loadNextQuestion() {
     var selectedOption = document.querySelector('input[type=radio]:checked');
-    if(!selectedOption) {
+    if (!selectedOption) {
         alert('Select your answer!');
         return;
     }
     var answer = selectedOption.value;
-    if(questions[currentQuestion].answer == answer) {
-        score ++;
+    if (questions[currentQuestion].answer == answer) {
+        score++;
     }
     selectedOption.checked = false;
     currentQuestion++;
-    if(currentQuestion == totalQuestions -1) {
+    if (currentQuestion == totalQuestions - 1) {
         nextButton.textContent = 'Finish';
     }
-    if(currentQuestion == totalQuestions) {
+    if (currentQuestion == totalQuestions) {
         container.style.display = 'none';
         resultCont.style.display = '';
         resultCont.textContent = 'your score' + score;
         return;
     }
+
     function run() {
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
-      }
-  
-      function decrement() {
+    }
+
+    function decrement() {
         number--;
         $("#time").html("<text>" + number + "<text>");
         if (number === 0) {
-          stop();
-          alert("Times Up!");
+            stop();
+            alert("Times Up!");
         }
         run(t);
-      }
+    }
     loadQuestion(currentQuestion);
 }
 loadQuestion(currentQuestion);
-
